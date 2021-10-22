3a854b73104505e3fb6c9513a4e6b4fc2cf42ffc
chriscaboverde <82997590+chriscaboverde@users.noreply.github.com>
2021-10-23 00:49:13 +0800

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,13 @@
+#string concatenation (aka how to put strings together)
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited because \
+ I love to {verb}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
+
+
